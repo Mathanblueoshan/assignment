1cf292d692cc7ed5e28ac2325d149ad67e7df422
Mathanblueoshan <Mathan@blueoshan.com>
2024-03-11 19:06:04 +0530

    viewmodel updated

--- lib/view_model/home.viewmodel.dart
+++ lib/view_model/home.viewmodel.dart
@@ -7,17 +7,28 @@ class HomeViewModel extends ChangeNotifier{
 
     init() {
    
+  
    
      
    }
+
+ List modelitems = [];
 
    //fetch the content from json
   Future<void> readJson() async {
     final String response = await rootBundle.loadString('assets/properties.json');
     debugPrint(response);
     final data = await json.decode(response);
+
+    modelitems = data;
  
  debugPrint(data);
+
+ setState(){
+
+   modelitems = data["items"];
+   
+ }
 
  notifyListeners();
   }
